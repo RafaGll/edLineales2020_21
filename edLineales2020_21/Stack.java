fd80df1b3e0a91b32cd69242e4b2cc3458c5838d
Rafael González <rafaelgonzalezllamas2001@gmail.com>
2020-11-19 17:21:08 +0100

    Update Stack.java

--- edLineales2020_21/Stack.java
+++ edLineales2020_21/Stack.java
@@ -3,7 +3,7 @@ package edLineales2020_21;
 /**
  * Interfaz para la creación de pilas estáticas y dinámicas
  */
-interface Stack <T> {
+public interface Stack <T> {
     public T pop ();
     public T top ();
     public void push (T element);
