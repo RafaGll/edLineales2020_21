b8f6655730ddecaecc0c008274636c5638f6dc55
Rafael González <rafaelgonzalezllamas2001@gmail.com>
2020-11-19 13:57:14 +0100

    Update QueueDynamic.java

--- edLineales2020_21/QueueDynamic.java
+++ edLineales2020_21/QueueDynamic.java
@@ -8,19 +8,20 @@ public abstract class QueueDynamic <T> implements Queue <T> {
     private Node <T> front, rear;	//Creación de los Nodos front y rear
 
     /**
-     * Método que crea una cola dinámica
-     * @param N Tamaño de la cola
-     * @param front Nodo front
-     * @param rear Nodo rear
+     * Constructor de colas dinámicas
+     * @param N La variable N almacena el tamaño de la cola (Se inicializa a 0)
+     * @param front Nodo front que contiene el primer elemento añadido y apunta al siguiente nodo
+     * @param rear Nodo rear que contiene el último nodo añadido y por tanto no apunta a ningún nodo
      */
     public QueueDynamic() {
         N = 0;
         front = new Node <T>();
         rear = new Node <T>();
-    }//Cierre del método
+    }//Cierre del constructor
 
     /**
      * Método que introduce un valor en la cola
+     * @param aux Nodo auxiliar que crearemos con el elemento deseado
      */
     public void enqueue(T element) {
         Node <T> aux = new Node<T> (element, null);
@@ -35,15 +36,16 @@ public abstract class QueueDynamic <T> implements Queue <T> {
     }//Cierre del método
 
     /**
-     * Método que elimina y devuelve el valor front de la cola
-     * En caso de que la cola está vacía, muestra un error
+     * Método que devuelve el valor front de la cola y lo elimina
+     * En caso de que la cola está vacía, muestra un error y devuelve un elemento vacío
      * @param element Elemento top de la pila
      * @return Elemento top de la pila
      */
     public T dequeue() {
     	T element = null;
     	if (isEmpty()) {
-    		return null;
+            System.out.println ("La pila está vacía");
+    		return element;
     	}else {
     		element = (T) front.getElement();
     		front = front.getNext();
@@ -55,15 +57,16 @@ public abstract class QueueDynamic <T> implements Queue <T> {
     }//Cierre del método
 
     /**
-     * Método que devuelve el valor front de la cola
-     * En caso de que la cola está vacía, muestra un error
+     * Método que devuelve el valor front de la cola sin eliminarlo
+     * En caso de que la cola está vacía, muestra un error y devuelve el elemento vacío
+     * @throws EmptyQueueException se lanza en caso de que la cola esté vacía
      * @return Elemento top de la pila
      */
-    public T front() {
+    public T front() throws EmptyQueueException {
         T element = null;
         try {
  		   element = (T) front.getElement();
- 	   } catch (EmptyStackException e) {
+ 	   } catch (EmptyQueueException e) {
  		   System.out.println ("La cola está vacía");
  	   }
         return element;
@@ -79,14 +82,20 @@ public abstract class QueueDynamic <T> implements Queue <T> {
 
     /**
      * Método que comprueba si una cola está vacía
-     * @return Toma el valor booleano False si la cola contiene algún valor y True si está vacía
+     * @return <ul>
+     *          <li>true: La cola está vacia</li>
+     *          <li>false: La cola contiene algún valor</li>
+     *          </ul>
      */
     public boolean isEmpty() {
         return N==0;
     }//Cierre del método
 
     /**
-     *
+     * Método que imprime todos los valores de la cola o indica que está vacía
+     * @param values Parametro en el que se almacenarán los valores 
+     * @param aux Nodo auxiliar para sacar uno a uno cada valor de la cola
+     * @return Valores de la pila
      */
     public String toString() {
         String values = "";
