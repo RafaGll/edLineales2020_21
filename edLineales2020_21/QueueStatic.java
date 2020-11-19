a9192370426d799f0807af3c5a98990a8f5a18db
Rafael González <rafaelgonzalezllamas2001@gmail.com>
2020-11-19 13:44:38 +0100

    Update QueueStatic.java

--- edLineales2020_21/QueueStatic.java
+++ edLineales2020_21/QueueStatic.java
@@ -10,22 +10,24 @@ public abstract class QueueStatic <T> implements Queue <T>{
     private T Q[];	//Creación de cola estática
 
     /**
-     * Método que crea una cola estática con un tamaño estandarizado de 10
-     * @param N El tamaño de la pila se inicia en 0
-     * @param front La posición front se iniciará en 0
-     * @param rear La posición rear se iniciará en 0
-     * @param Q Cola
+     * Constructor de colas estáticas con un tamaño estandarizado de 10
+     * @param N La variable N almacena el tamaño de la cola (Se iniciaiza a 0)
+     * @param front La posición front (primer valor/cabeza) se iniciará en 0
+     * @param rear La posición rear (último valor/cola/final) se iniciará en 0
+     * @param Q Cola estática
      */
     public QueueStatic () {
         N = 0;
         front = 0;
         rear = 0;
         Q = (T[]) new Object[10];
-    }//Cierre del método
+    }//Cierre del constructor
 
     /**
      * Método que introduce un valor en la cola
-     * En caso de que la cola está llena, muestra un error
+     * En caso de que la cola esté llena se redimensionará la cola añadiendo el valor
+     * @param element Valor a introducir en la cola
+     * @throws FullQueueException se lanza en caso de que la cola esté llena
      */
     public void enqueue (Object e) throws FullQueueException {
         T element = (T) e;
@@ -40,9 +42,10 @@ public abstract class QueueStatic <T> implements Queue <T>{
     }//Cierre del método
 
     /**
-     * Método que saca el valor front de la cola
-     * En caso de que la cola está vacía, muestra un error
+     * Método que devuelve el valor front de la cola y lo elimina de la misma
+     * En caso de que la cola está vacía, muestra un error y devuelve el elemento vacío
      * @param element Elemento front de la cola
+     * @throws EmptyQueueException se lanza en caso de que la cola esté vacía
      * @return Elemento front de la cola
      */
     public T dequeue () throws EmptyQueueException {
@@ -60,6 +63,7 @@ public abstract class QueueStatic <T> implements Queue <T>{
     /**
      * Método que devuelve el valor front de la cola
      * En caso de que la cola está vacía, muestra un error
+     * @throws EmptyQueueException se lanza en caso de que la cola esté vacía
      * @return Elemento front de la cola
      */
     public T front () throws EmptyQueueException {
@@ -81,15 +85,21 @@ public abstract class QueueStatic <T> implements Queue <T>{
     }//Cierre del método
 
     /**
-     * Método que comprueba si una pila está vacía
-     * @return Toma el valor booleano False si la pila contiene algún valor y True si está vacía
+     * Método que comprueba si una cola está vacía
+     * @return <ul>
+     *          <li>true: La cola está vacia</li>
+     *          <li>false: La cola contiene algún valor</li>
+     *          </ul>
      */
     public boolean isEmpty() {
         return N == 0;
     }//Cierre del método
 
     /**
-     *
+     * Método que imprime todos los valores de la cola o indica que está vacía
+     * @param values Parametro en el que se almacenarán los valores 
+     * @param aux Valor auxiliar para sacar uno a uno cada valor de la cola
+     * @return Valores de la pila
      */
     public String toString() {
         String values = "";
