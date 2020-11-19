eed424b05ee1b3ffc6ff9a9cfa8358819c1b8e54
Rafael González <rafaelgonzalezllamas2001@gmail.com>
2020-11-19 18:12:09 +0100

    no he hecho nada

--- edLineales2020_21/Nodo.java
+++ edLineales2020_21/Nodo.java
@@ -1,18 +1,19 @@
 package edLineales2020_21;
+
 /**
  * Esta clase crea y modifica un nodo
  */
 class Node<T> {
     T element;	//Elemento de la pila
     Node<T> next;	//Creación del Nodo next
-    
+
     /**
      * Método que crea un Nodo que apunta a null
      */
     public Node () {
         next = null;
     }//Cierre del método
-    
+
     /**
      * Método que crea un Nodo que apunta al elemento Nodo next y guarda el siguiente valor element en el Nodo actual
      * @param element Elemento al que se refiere el Nodo
@@ -22,7 +23,7 @@ class Node<T> {
     	this.element = element;
         this.next = next;
     }//Cierre del método
-    
+
     /**
      * Método que devuelve el valor element de un Nodo
      * @return Elemento del Nodo
@@ -30,7 +31,7 @@ class Node<T> {
 	public T getElement () {
         return element;
     }//Cierre del método
-	
+
 	/**
      * Método que devuelve el valor element del Nodo next
      * @return Elemento del Nodo next
@@ -38,14 +39,14 @@ class Node<T> {
     public Node<T> getNext () {
         return next;
     }//Cierre del método
-    
+
     /**
      * Método que inserta un valor element en un Nodo
      */
     public void setElement (T element) {
         this.element = element;
     }//Cierre del método
-    
+
     /**
      * Método que inserta un valor element en el Nodo next
      */
